4dce39f748c34b9b08883d82928321d29f7c0620
Mackinlay Kevin <kemack83@gmail.com>
2023-08-04 20:55:11 -0300

    class, function and elements

--- js/archivo.js
+++ js/archivo.js
@@ -1,20 +1,41 @@
-// elementos del Html
+//  traemos los elementos del Html
 
-const nombre =  document.querySelector("#nombre");
+const form = document.querySelector("#form");
+const nombre = document.querySelector("#nombre");
 const puesto = document.querySelector("#puesto");
 const edad = document.querySelector("#edad");
 const btn = document.querySelector("#btn");
 const buscar = document.querySelector("#buscar");
 const contenedorEmpleados = document.querySelector("#contenedorEmpleados");
 
-
-class Empleador{
-    constructor(nombre, puesto, edad) {
-        this.id = Date.now().toString(36); //genero un id ùnico
-        this.nombre = nombre;
-        this.puesto = puesto;
-        this.edad = edad;
-    }
+class Empleado {
+  constructor(nombre, puesto, edad) {
+    this.id = Date.now().toString(36); //genero un id ùnico
+    this.nombre = nombre;
+    this.puesto = puesto;
+    this.edad = edad;
+  }
 }
 
-let empleado = [new Empleador("Silvio", "jr", 30)];
+let empleados = [];
+
+form.onsubmit = (event) => {
+  // Prevenimos el comportamiento por defecto que tienen los formularios de recargarse la página
+  event.preventDefault();
+  empleados.push(new Empleado(nombre.value, puesto.value, edad.value));
+  console.log(empleados);
+};
+
+const mostrarEmpleados = (empleados) => {
+  // Borramos el html para poner el array actualizado
+  contenedorEmpleados.innerHTML = "";
+
+  empleados.forEach((empleado, index) => {
+    let empleadosContenedor = document.createElement("div");
+    empleadosContenedor.classList.add("mt-2", "border", "border-2", "p-3", "shadow", "shadow-md");
+    empleadosContenedor.innerHTML = `
+    <p>Nombre: ${empleado.nombre}</p>
+    <p>Puesto: ${empleado.puesto}</p>
+    <p>Edad: ${empleado.edad}</p>`;
+  });
+};
